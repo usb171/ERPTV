aee379cd506fe9a6b5201289ef76ccfa939c2f62
Nilton <usb171@gmail.com>
2018-08-24 13:47:17 -0300

    Criado as classes Função, Profile. Falta implementar filtros

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Profile, Funcao
 
-# Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['nomeCompleto', 'funcao', 'matricula', 'dataAdmissao']
+    readonly_fields = ['acesso']
+
+class FuncaoAdmin(admin.ModelAdmin):
+    list_display = ['nome']
+
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Funcao, FuncaoAdmin)
