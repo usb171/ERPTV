e2e5bdd2ffccd07abe3429b75a9f22aa8ecf3098
Nilton <usb171@gmail.com>
2018-08-24 14:36:25 -0300

    Implementado Filtro por nome

--- core/admin.py
+++ core/admin.py
@@ -5,8 +5,18 @@ class ProfileAdmin(admin.ModelAdmin):
     list_display = ['nomeCompleto', 'funcao', 'matricula', 'dataAdmissao']
     readonly_fields = ['acesso']
 
+    search_fields = (
+        'nomeCompleto',
+    )
+
+
+
 class FuncaoAdmin(admin.ModelAdmin):
     list_display = ['nome']
+
+    search_fields = (
+        'nome',
+    )
 
 
 admin.site.register(Profile, ProfileAdmin)
